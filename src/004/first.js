0caebda76d0b2a10dba38a75d3a19871a8ca6e1b
Dan Gilleland <dgilleland@nait.ca>
2024-09-11 14:08:28 -0600

    Commenting on loading JavaScript in the browser

--- src/004/first.js
+++ src/004/first.js
@@ -1,1 +1,8 @@
 alert('I am about to load the body of this page.');
+// JavaScript can "block" rendering. The line above is an
+// example of this. This script is loaded and executed
+// immediately by the browser because the script is loaded
+// in the <head> part of the HTML.
+// Because we're now waiting for the user to close the
+// alert box, the browser is prevented from rendering the
+// <body>.
