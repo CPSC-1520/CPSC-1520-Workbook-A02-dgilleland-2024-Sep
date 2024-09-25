87605920d617106e9f0d8c211b5007075b96f08e
Dan Gilleland <dgilleland@nait.ca>
2024-09-25 13:19:36 -0600

    Ad-hoc if/else statements

--- src/012/demo-grammar/main.js
+++ src/012/demo-grammar/main.js
@@ -21,3 +21,26 @@ Relational Operators
 <=      less than or equal to
 
 */
+
+// Explore a bit with some variables and values
+let numberOne = 1;
+let numberEight = 8;
+let textEight = '8';
+let name = 'Dan';
+let temp; // I'll leave this undefined.
+
+// Some 'if/else' statements
+if(numberEight !== textEight) {
+    console.log('The results don\'t match because they are different data types');
+}
+if(numberEight == textEight) {
+    console.log('loose comparison where automatic type conversion is taking place.');
+}
+if(numberEight) {
+    console.log(`${numberEight} is a non-zero value, therefore is 'truthy'`);
+}
+if(temp) {
+    console.log('temp has a value');
+} else {
+    console.log(`temp variable is ${temp}`);
+}
