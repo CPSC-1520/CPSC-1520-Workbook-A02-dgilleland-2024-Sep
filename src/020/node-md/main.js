d3ef1a4392846ad40c2d6c33891685b6a23cd35e
Dan Gilleland <dgilleland@nait.ca>
2024-10-24 08:49:39 -0600

    Fetch markdown from the server

--- src/020/node-md/main.js
+++ src/020/node-md/main.js
@@ -1,1 +1,18 @@
 console.log('loaded main.js');
+
+/**
+ * 
+ * @param {Response} response 
+ */
+const handleResponse = function(response) {
+    console.log(response.status, response.statusText);
+    return response.text();
+}
+
+const handleMarkdown = function(markdownText) {
+    console.log(markdownText);
+}
+
+fetch('./content/ReadMe.md')
+    .then(handleResponse)
+    .then(handleMarkdown);
