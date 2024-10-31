84ad0ec8f78b2b1d58bf3847229c6f6c9f9b6012
Dan Gilleland <dgilleland@nait.ca>
2024-10-31 07:33:39 -0600

    Complete timers/intervals

--- src/024/demos/ExploringIntervals/main.js
+++ src/024/demos/ExploringIntervals/main.js
@@ -6,16 +6,19 @@ const addDots = function() {
 
 // Start an interval
 let cancellationRef = setInterval(addDots, 1250);
+console.log('Interval ref:', cancellationRef);
 
 // Add an event listener to the button that will stop the interval.
 document.querySelector('button')
         .addEventListener('click', function(evt) {
-            if(cancellationRef) {
+            if(cancellationRef) { // as long as cancellationRef is "truthy", we can stop the interval
                 clearInterval(cancellationRef);
                 cancellationRef = 0;
                 evt.target.innerText = "Continue Interval";
             } else {
+                // We start up a NEW interval
                 cancellationRef = setInterval(addDots, 1250);
                 evt.target.innerText = "Stop Interval";
+                console.log('Interval ref:', cancellationRef);
             }
         })
